00dd724a4748d9d851b7b0a553497eed78f48cd7
bascode-dev <bascode-dev@gmail.com>
2025-01-29 10:53:18 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div className='app'>
+       <h1>React Application</h1>
+       <hr />
+       <p>created by <b>bascode-dev</b></p>
+    </div>
+  )
+}
+
+export default App
